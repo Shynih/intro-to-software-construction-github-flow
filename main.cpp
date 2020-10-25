e731af5876abd9ac8ed4ec8893bd80099fdfc1d4
nickthechang <nchan080@ucr.edu>
2020-10-25 16:17:35 -0700

    Revert "changed main.cpp"
    This reverts commit 986ce793c634fe969f0d459be492b88d1ad99cc8.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include "c-echo.h"
+#include <iostream>
+#include <string>
 
-int main(int argv, char** argc) {
-    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
+int main()
+{
+<<<<<<< HEAD
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
+
+    return 0;
+=======
+	std::string name;
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> name;
+	std::cout << "Hello " << name << "!" << std::endl;
+	
+	return 0;
+>>>>>>> 10de46c37df7e335149ec5b43da7c26d509b5b94
 }
