645b5851d04da52cdf6a503e12c83f54e17422e1
nickthechang <nchan080@ucr.edu>
2020-10-25 15:51:46 -0700

    Fixes #1
     Please enter the commit message for your changes. Lines starting

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
 #include <string>
+int main(){
 
-int main()
-{
-<<<<<<< HEAD
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
 
-    return 0;
-=======
 	std::string name;
 	std::cout << "What is your name?" << std::endl;
 	std::cin >> name;
 	std::cout << "Hello " << name << "!" << std::endl;
 	
 	return 0;
->>>>>>> 10de46c37df7e335149ec5b43da7c26d509b5b94
+
 }
